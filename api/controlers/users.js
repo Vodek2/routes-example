78e653706f97a0d6f97bfaed216f1dd4cd7d48b3
Wlodzimierz Kwasniak <vodek4791@tiscali.co.uk>
2017-12-03 18:28:42 +0000

    added users model

--- api/controlers/users.js
+++ api/controlers/users.js
@@ -1,19 +1,28 @@
+const usersModel = require('../models/users')
+
 const users = {
     getAllUsers:(req,res)=>{
         return res.status(200).json({
-            message:'Get from users controler'
+            users:usersModel,
+            message:'Get from users controller'
         })
     },
     postUsers:(req, res)=>{
         return res.status(200).json({
-            message:'Post from users controler'
+            message:'Post from users controller'
         })
     },
     getSingleUser:(req, res)=>{
-
         const id = req.params.userId
+        let userResp= usersModel.filter((user)=>{
+            if(user.id == id){
+                return user
+            }
+        })
+
         return res.status(200).json({
-            message:'Get from users controler '+id
+            user:userResp[0],
+            message:'Get from users controller, requested id =  '+id
         })
 
     }
---
+++ api/models/users.js
@@ -0,0 +1,17 @@
+
+const users = [
+    {id:1,
+    name:'vodek'},
+    {id:2,
+    name:'Shan'},
+    {id:3,
+    name:'Gyula'}
+    ]
+
+module.exports = users
+
+
+
+
+
+
